f37fe81b2af25eed3dee96424429eda343adb7ac
renansui <renanddtao@gmail.com>
2022-10-19 23:03:25 -0300

    update javascript, added show, hide and reset functions

--- src/app.ts
+++ src/app.ts
@@ -14,36 +14,71 @@ const hideFormBtn = document.querySelector('#hideFormBtn') as HTMLButtonElement;
 myForm.addEventListener('submit', (e): void => {
 	e.preventDefault();
 
-	// form data
-	const title = myFormTitle.value;
-	const amount = parseInt(myFormAmount.value);
-	const amountString = parseInt(myFormAmount.value).toLocaleString('en-US');
-	const note = myFormNote.value;
-	const type = myFormType.value;
+	// Reset form
+	RESET_Form();
 
-	console.log(amount);
+	// Remove form
+	SHOW_HIDE_FormBtn('hide');
 });
+
 // drop down menu on the header title: all income or all expense or total balance
 // make the balance, income and expense be updated automatically using localStorage
 // click event on balance, income, expense or individual transactions and filter localStorage result
-// dark and white theme
 
-// Show and hide form
+// Get the data - fazer amanhã
+interface Transaction {
+	title: string;
+	amountNumber: number;
+	note: string;
+	type: string;
+	amountString: string;
+}
+const getFormData = (): void => {
+	// form data
+	const title = myFormTitle.value;
+	const amountNumber: number = parseInt(myFormAmount.value); // number
+	const note = myFormNote.value;
+	const type = myFormType.value;
+	const amountString: string = parseInt(myFormAmount.value).toLocaleString(
+		'en-US'
+	); // string
+
+
+};
+
+// SHOW, HIDE and RESET Form
 showFormBtn.addEventListener('click', (): void => {
-	const form = myForm.classList;
-
-	// Show form
-	form.remove('hide');
-	form.add('show');
-
-	// Show form if do not contain hide and show class
-	if (!form.contains('hide') || !form.contains('show')) form.add('show');
+	SHOW_HIDE_FormBtn('show');
 });
 
 hideFormBtn.addEventListener('click', (): void => {
+	SHOW_HIDE_FormBtn('hide');
+});
+
+const SHOW_HIDE_FormBtn = (action: string): void => {
+	// get the classList of the form
 	const form = myForm.classList;
 
-	// Remove form
-	form.remove('show');
-	form.add('hide');
-});
+	// Show form if do not contain hide and show class
+	if (!form.contains('hide') || !form.contains('show')) form.add('show');
+
+	// show or hide conditionals
+	if (action === 'show') {
+		// Show form
+		form.remove('hide');
+		form.add('show');
+	} else {
+		// Remove form
+		form.remove('show');
+		form.add('hide');
+	}
+};
+
+const RESET_Form = (): void => {
+	myFormTitle.value = '';
+	myFormAmount.value = '';
+	myFormNote.value = '';
+	myFormType.value = '';
+};
+
+// dark and white theme
