75bbf71bacbd81608cb4a923717e0eabb3ab09ff
renansui <renanddtao@gmail.com>
2023-08-07 09:53:21 -0300

    added next

---
+++ src/app/(lobby)/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      <header className="m-4 h-full max-h-full rounded-md border border-neutral-700">
+        ABC
+      </header>
+      <main className="m-4 h-full max-h-full rounded-md border border-neutral-700">
+        ABC
+      </main>
+    </>
+  )
+}
