58a5735a5dbf2761f9efecc4700761cceec5886b
RenanSui <renanddtao@gmail.com>
2023-11-09 12:58:06 -0300

    add more utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const getObjFromEntries = (
+  iterableObj: Iterable<readonly [PropertyKey, unknown]>,
+) => {
+  const obj = JSON.parse(JSON.stringify(Object.fromEntries(iterableObj)))
+  return obj
+}
