b70e4c1ef5946aab27185bfeb265af6cefcbb8ab
RenanSui <renanddtao@gmail.com>
2023-11-09 13:02:40 -0300

    rename site-header

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,6 +1,6 @@
 import { getUser } from '@/actions/server/user'
+import Image from 'next/image'
 import Link from 'next/link'
-import { UserDropdown } from './UserDropdown'
 import { buttonVariants } from './ui/button'
 
 export const SiteHeader = async () => {
@@ -9,7 +9,19 @@ export const SiteHeader = async () => {
   return (
     <header className="flex items-center justify-between gap-2 border-b-[1px] border-neutral-900 px-8 py-4 md:justify-end">
       {user ? (
-        <UserDropdown />
+        <>
+          <Image
+            src={user.image ?? ''}
+            alt={'profile pic'}
+            className="rounded-full"
+            width={32}
+            height={32}
+          />
+          {/* <UserDropdown /> */}
+          <Link className={buttonVariants({ size: 'sm' })} href={'/signout'}>
+            Sign out
+          </Link>
+        </>
       ) : (
         <Link className={buttonVariants({ size: 'sm' })} href={'/signin'}>
           Sign in
