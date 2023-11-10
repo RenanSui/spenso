de1870cde7b3b60e0e3c25a8ff5483950fc41024
RenanSui <renanddtao@gmail.com>
2023-11-10 15:57:03 -0300

    update user server actions

--- src/actions/server/user.ts
+++ src/actions/server/user.ts
@@ -1,12 +1,29 @@
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 import { createServerClient } from '@/lib/server/server'
-import { User } from 'next-auth'
+import { getServerSession } from 'next-auth'
+
+export type SessionUser = {
+  name: string | null | undefined
+  email: string | null | undefined
+  image: string | null | undefined
+}
 
 export const getUser = async () => {
+  const session = await getServerSession(authOptions)
+  if (!session) return null
+
+  const { address, ...user } = session.user
+  String(address) // disable error
+
+  return user as SessionUser
+}
+
+export const getUserId = async () => {
   const supabase = await createServerClient()
   if (!supabase) return null
 
-  const { data: user } = await supabase.from('users').select('*')
+  const { data: user } = await supabase.from('users').select('id')
   if (!user) return null
 
-  return user[0] as User
+  return user[0].id as string
 }
