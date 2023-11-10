48ec60a504da7f8c3009ba017526384bd8de0699
RenanSui <renanddtao@gmail.com>
2023-11-10 15:54:06 -0300

    add session-provider to app/layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import { Grainy } from '@/components/grainy'
+import SessionProvider from '@/components/session-provider'
 import { cn } from '@/lib/utils'
 import type { Metadata } from 'next'
+import { getServerSession } from 'next-auth'
 import { Inter } from 'next/font/google'
 import '../styles/globals.css'
+import { authOptions } from './api/auth/[...nextauth]/route'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,15 +14,17 @@ export const metadata: Metadata = {
   description: 'Tracking where your money is going.',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const session = await getServerSession(authOptions)
+
   return (
     <html lang="en">
-      <body className={cn('bg-black text-white', inter.className)}>
-        {children}
+      <body className={cn('bg-neutral-950 text-white', inter.className)}>
+        <SessionProvider session={session}>{children}</SessionProvider>
         <Grainy />
       </body>
     </html>
