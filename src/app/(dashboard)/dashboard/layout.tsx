f70299e4af874c8a3b28fd06aa01d8fea8e1cb86
RenanSui <renanddtao@gmail.com>
2023-11-11 02:32:39 -0300

    change border color

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -15,7 +15,7 @@ export default async function DashboardLayout({
     <div className="flex min-h-screen flex-col">
       <SiteHeader user={user} />
       <div className="mx-auto w-full max-w-[1440px] flex-1 items-start px-4 sm:px-8 md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
-        <aside className="fixed top-14 z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 overflow-y-auto border-r border-neutral-800 md:sticky md:block">
+        <aside className="fixed top-14 z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 overflow-y-auto border-r border-neutral-900 md:sticky md:block">
           <ScrollArea className="py-6 pr-6 lg:py-8">
             <SidebarNav items={dashboardConfig.SidebarNav} className="p-1" />
           </ScrollArea>
