a247716a165e218ef9ba5b92c6c4a6e6112e3336
RenanSui <renanddtao@gmail.com>
2023-11-15 08:36:17 -0300

    update tailwin to use class

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  darkMode: ['class', '[data-theme="dark"]'],
+  darkMode: 'class', // 'data-theme="dark"'
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     container: {
