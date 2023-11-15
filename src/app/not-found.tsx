bac81bbd6a80c7757aa5beb1a6377a6acc0feb88
RenanSui <renanddtao@gmail.com>
2023-11-15 07:08:55 -0300

    update not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,7 @@
 import { getUser } from '@/actions/server/user'
+import { ErrorCard } from '@/components/cards/error-card'
 import { SiteHeader } from '@/components/layouts/site-header'
+import { Shell } from '@/components/shells/shell'
 
 export default async function PageNotFound() {
   const user = await getUser()
@@ -7,6 +9,14 @@ export default async function PageNotFound() {
   return (
     <>
       <SiteHeader user={user} />
+      <Shell variant="centered">
+        <ErrorCard
+          title="Page not found"
+          description="The page you are looking for does not exist"
+          retryLink="/"
+          retryLinkText="Go to Home"
+        />
+      </Shell>
     </>
   )
 }
