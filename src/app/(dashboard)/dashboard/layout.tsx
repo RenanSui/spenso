5804eb30acd9998d9f4586579be4580c51185e4c
RenanSui <renanddtao@gmail.com>
2023-11-15 09:00:18 -0300

    add site-footer to dashboard layout

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import { getUser } from '@/actions/server/user'
 import { SidebarNav } from '@/components/layouts/sidebar-nav'
+import { SiteFooter } from '@/components/layouts/site-footer'
 import { SiteHeader } from '@/components/layouts/site-header'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { dashboardConfig } from '@/config/dashboard'
@@ -12,7 +13,7 @@ export default async function DashboardLayout({
   const user = await getUser()
 
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="relative flex min-h-screen flex-col">
       <SiteHeader user={user} />
       <div className="mx-auto w-full max-w-[1440px] flex-1 items-start px-4 sm:px-8 md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
         <aside className="fixed top-14 z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 overflow-y-auto border-r border-neutral-900 md:sticky md:block">
@@ -22,6 +23,7 @@ export default async function DashboardLayout({
         </aside>
         <main className="flex w-full flex-col overflow-hidden">{children}</main>
       </div>
+      <SiteFooter />
     </div>
   )
 }
