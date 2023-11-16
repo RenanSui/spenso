2ef59d7ac16592c38ad72986c7e1bb9a2b7fefaa
RenanSui <renanddtao@gmail.com>
2023-11-16 03:38:33 -0300

    add unsplash links to config/site

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,4 +3,16 @@ const links = {
   githubAccount: 'https://github.com/RenanSui',
 }
 
-export const siteConfig = { links }
+const unsplash = {
+  auth: {
+    imageUrl:
+      'https://images.unsplash.com/photo-1566866856854-a0b3d69a62c0?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    author: 'Brian Jones',
+    authorUrl:
+      'https://unsplash.com/@briannjoness?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+    imagePageUrl:
+      'https://unsplash.com/photos/curtain-buildings-at-night-JFpLW-xhvco?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+  },
+}
+
+export const siteConfig = { links, unsplash }
