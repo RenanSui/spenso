80f40521882c2d45bdec97624ded848a36e62c08
RenanSui <renanddtao@gmail.com>
2023-11-16 05:36:04 -0300

    add lobby to config site

--- src/config/site.ts
+++ src/config/site.ts
@@ -13,6 +13,16 @@ const unsplash = {
     imagePageUrl:
       'https://unsplash.com/photos/curtain-buildings-at-night-JFpLW-xhvco?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
   },
+
+  lobby: {
+    imageUrl:
+      'https://images.unsplash.com/photo-1554224155-a1487473ffd9?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    author: 'Kelly Sikkema',
+    authorUrl:
+      'https://unsplash.com/@kellysikkema?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+    imagePageUrl:
+      'https://unsplash.com/photos/coffee-mug-near-open-folder-with-tax-withholding-paper-wgcUx0kR1ps?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+  },
 }
 
 export const siteConfig = { links, unsplash }
