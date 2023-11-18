0d8f925efe44a6640f05d4e6a4b9dd8cd8aa765b
RenanSui <renanddtao@gmail.com>
2023-11-18 08:24:18 -0300

    lib/transactions

---
+++ src/lib/transactions.ts
@@ -0,0 +1,9 @@
+import { TransactionTypes } from '@/types'
+
+export const transactionTypeses: {
+  label: string
+  value: TransactionTypes
+}[] = [
+  { label: 'Income', value: 'income' },
+  { label: 'Expense', value: 'expense' },
+]
