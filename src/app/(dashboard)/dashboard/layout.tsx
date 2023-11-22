542c528b80f7525dbc6e8c3390cfec0675eca443
RenanSui <renanddtao@gmail.com>
2023-11-22 15:05:27 -0300

    fix layout position again

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -18,7 +18,7 @@ export default async function DashboardLayout({
     <div className="relative flex min-h-screen flex-col">
       <SiteHeader user={user} />
       <div className="mx-auto w-full max-w-[1440px] flex-1 items-start px-4 sm:px-8 md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
-        <aside className=" top-14 z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 overflow-y-auto border-r dark:border-neutral-900 md:block">
+        <aside className="-ml-2 hidden h-full border-r dark:border-neutral-900 md:block">
           <ScrollArea className="py-6 pr-6 lg:py-8">
             <SidebarNav items={dashboardConfig.SidebarNav} className="p-1" />
           </ScrollArea>
