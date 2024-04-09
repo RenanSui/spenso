3b1a2d972a10e8f03d9c5c9d11e58897559529c9
RenanSui <renanddtao@gmail.com>
2024-04-09 14:04:01 -0300

    center not-found component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ import { Shell } from '@/components/shells/shell'
 export default async function PageNotFound() {
   return (
     <>
-      <Shell variant="centered">
+      <Shell className="mx-auto max-w-[1440px]" variant="centered">
         <ErrorCard
           title="Page not found"
           description="The page you are looking for does not exist"
