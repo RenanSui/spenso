24e04dd506e5c423271ec38d070b6da24b212774
RenanSui <renanddtao@gmail.com>
2024-05-06 16:04:14 -0300

    add menu icon

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -45,4 +45,11 @@ export const Icons = {
       ></path>
     </svg>
   ),
+  menu: ({ ...props }: IconProps) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" strokeWidth="1.5" {...props}>
+      <path d="M3 5H11" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+      <path d="M3 12H16" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+      <path d="M3 19H21" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+    </svg>
+  ),
 }
