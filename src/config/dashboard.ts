776dd5d879bd8de4683de14901e3a6633f07c4a6
RenanSui <renanddtao@gmail.com>
2024-05-06 16:04:02 -0300

    comment unused dashboard items

--- src/config/dashboard.ts
+++ src/config/dashboard.ts
@@ -3,27 +3,35 @@ import { DashboardConfig, TransactionTypes } from '@/types'
 export const dashboardConfig: DashboardConfig = {
   SidebarNav: [
     {
+      title: 'Groups',
+      href: '/dashboard/groups',
+      disabled: false,
+      icon: 'listBulletIcon',
+      description: 'Manage your groups',
+      items: [],
+    },
+    // {
+    //   title: 'Transactions',
+    //   href: '/dashboard/transactions',
+    //   disabled: false,
+    //   icon: 'listBulletIcon',
+    //   description: 'Manage your transactions',
+    //   items: [],
+    // },
+    // {
+    //   title: 'Analytics',
+    //   href: '/dashboard/analytics',
+    //   disabled: false,
+    //   icon: 'dashboardIcon',
+    //   description: 'Analyze your transactions',
+    //   items: [],
+    // },
+    {
       title: 'Account',
       href: '/dashboard/account',
       disabled: true,
       icon: 'avatarIcon',
       description: 'Manage your account settings',
-      items: [],
-    },
-    {
-      title: 'Analytics',
-      href: '/dashboard/analytics',
-      disabled: false,
-      icon: 'dashboardIcon',
-      description: 'Analyze your transactions',
-      items: [],
-    },
-    {
-      title: 'Transactions',
-      href: '/dashboard/transactions',
-      disabled: false,
-      icon: 'listBulletIcon',
-      description: 'Manage your transactions',
       items: [],
     },
     {
