ceeb060076a2d59a2d2140e6281a97b64a24b999
RenanSui <renanddtao@gmail.com>
2024-05-06 07:56:36 -0300

    modify signout page style

--- src/app/(auth)/signout/page.tsx
+++ src/app/(auth)/signout/page.tsx
@@ -1,62 +1,18 @@
 import { Signout } from '@/components/auth/sign-out'
-import { buttonVariants } from '@/components/ui/button'
-import { siteConfig } from '@/config/site'
-import { authOptions } from '@/lib/auth'
-import { cn } from '@/lib/utils'
-import { getServerSession } from 'next-auth'
-import Image from 'next/image'
-import Link from 'next/link'
-import { redirect } from 'next/navigation'
+import { PageHeader, PageHeaderDescription, PageHeaderHeading } from '@/components/page-header'
+import { Shell } from '@/components/shells/shell'
 
 export default async function Page() {
-  const session = await getServerSession(authOptions)
-  if (!session?.user) redirect('/signin')
+  // const session = await getServerSession(authOptions)
+  // if (!session?.user) redirect('/signin')
 
   return (
-    <section className="flex bg-white text-white dark:bg-black">
-      <div className="absolute bottom-0 left-0 right-0 top-0 w-screen md:relative">
-        <Image
-          className="absolute inset-0 h-full w-full object-cover text-transparent opacity-50"
-          src={siteConfig.unsplash.auth.imageUrl}
-          width={1280}
-          height={1080}
-          alt={'photo of city buildings during daytime'}
-        />
-
-        <div className="relative z-10 flex h-full flex-col justify-between p-4 px-8">
-          <Link className="text-2xl font-semibold tracking-tight text-black dark:text-white" href="/">
-            Spenso
-          </Link>
-          <p className="text-black dark:text-white">
-            Photo by{' '}
-            <a className="hover:underline" href={siteConfig.unsplash.auth.authorUrl} target="_blank">
-              {siteConfig.unsplash.auth.author}
-            </a>{' '}
-            on{' '}
-            <a className="hover:underline" href={siteConfig.unsplash.auth.imagePageUrl} target="_blank">
-              Unsplash
-            </a>
-          </p>
-        </div>
-      </div>
-
-      <div className="relative z-10 flex h-screen w-screen items-center justify-center p-8">
-        <div className="w-full max-w-xs rounded-xl bg-transparent p-6 shadow xl:mx-8">
-          <div className="flex flex-col items-center space-y-1 pb-6">
-            <h1 className="text-2xl font-semibold tracking-tight text-black dark:text-white">Sign out</h1>
-            <p className="w-[150px] text-center text-neutral-800 dark:text-zinc-400">
-              Are you sure you want to sign out?
-            </p>
-          </div>
-
-          <div className="flex justify-center space-x-2">
-            <Signout className="w-full">Log out</Signout>
-            <Link className={cn(buttonVariants({ size: 'sm', variant: 'outline' }), 'w-full')} href={'/'}>
-              Go back
-            </Link>
-          </div>
-        </div>
-      </div>
-    </section>
+    <Shell className="container max-w-md rounded-xl border border-neutral-200 bg-white shadow dark:border-neutral-800 lg:bg-transparent">
+      <PageHeader className="text-center">
+        <PageHeaderHeading size="sm">Sign out</PageHeaderHeading>
+        <PageHeaderDescription size="sm">Are you sure you want to sign out?</PageHeaderDescription>
+      </PageHeader>
+      <Signout />
+    </Shell>
   )
 }
