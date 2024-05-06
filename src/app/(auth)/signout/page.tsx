162885e60f2d130519a4ea52d662dc1be3f78466
RenanSui <renanddtao@gmail.com>
2024-05-06 08:31:44 -0300

    modify auth signout page style

--- src/app/(auth)/signout/page.tsx
+++ src/app/(auth)/signout/page.tsx
@@ -1,13 +1,16 @@
 import { Signout } from '@/components/auth/sign-out'
 import { PageHeader, PageHeaderDescription, PageHeaderHeading } from '@/components/page-header'
 import { Shell } from '@/components/shells/shell'
+import { authOptions } from '@/lib/auth'
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
 
 export default async function Page() {
-  // const session = await getServerSession(authOptions)
-  // if (!session?.user) redirect('/signin')
+  const session = await getServerSession(authOptions)
+  if (!session?.user) redirect('/signin')
 
   return (
-    <Shell className="container max-w-md rounded-xl border border-neutral-200 bg-white shadow dark:border-neutral-800 lg:bg-transparent">
+    <Shell className="container max-w-md rounded-xl text-neutral-950 dark:text-neutral-50 lg:bg-transparent">
       <PageHeader className="text-center">
         <PageHeaderHeading size="sm">Sign out</PageHeaderHeading>
         <PageHeaderDescription size="sm">Are you sure you want to sign out?</PageHeaderDescription>
