4b54788a1c07bbcf825315f4e949fe216b5f1a1b
RenanSui <renanddtao@gmail.com>
2024-05-06 16:06:15 -0300

    modify data table style

--- src/components/data-table/data-table-faceted-filter.tsx
+++ src/components/data-table/data-table-faceted-filter.tsx
@@ -84,13 +84,13 @@ export function DataTableFacetedFilter<TData, TValue>({
                   >
                     <div
                       className={cn(
-                        'border-primary mr-2 flex h-4 w-4 items-center justify-center rounded-sm border',
+                        'mr-2 flex h-4 w-4 items-center justify-center rounded-sm border border-primary',
                         isSelected ? 'bg-primary text-primary-foreground' : 'opacity-50 [&_svg]:invisible',
                       )}
                     >
                       <CheckIcon className={cn('h-4 w-4')} aria-hidden="true" />
                     </div>
-                    {option.icon && <option.icon className="text-muted-foreground mr-2 h-4 w-4" aria-hidden="true" />}
+                    {option.icon && <option.icon className="mr-2 h-4 w-4 text-muted-foreground" aria-hidden="true" />}
                     <span>{option.label}</span>
                   </CommandItem>
                 )
