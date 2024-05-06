963327bf70cbe655d1438db082736a660194f163
RenanSui <renanddtao@gmail.com>
2024-05-06 20:31:07 -0300

    create currency provider

---
+++ src/components/providers/currency-provider.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { Provider, createStore, useAtomValue } from 'jotai'
+import { atomWithStorage } from 'jotai/utils'
+
+const store = createStore()
+export const currencyAtom = atomWithStorage('currency', 'BRL')
+
+export const CurrencyProvider = ({ children }: React.HTMLAttributes<HTMLDivElement>) => (
+  <Provider store={store}>{children}</Provider>
+)
+
+export const useCurrencyAtom = () => {
+  return useAtomValue(currencyAtom)
+}
