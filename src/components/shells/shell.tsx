e7da5f41d13074f7f94b001f3f79575699a97481
RenanSui <renanddtao@gmail.com>
2024-05-07 10:13:51 -0300

    modify shell component style

--- src/components/shells/shell.tsx
+++ src/components/shells/shell.tsx
@@ -4,9 +4,10 @@ import { VariantProps, cva } from 'class-variance-authority'
 const shellVariants = cva('grid items-center gap-8 pb-8 pt-6 md:py-8', {
   variants: {
     variant: {
-      default: '',
-      centered: 'p-8 flex h-[100dvh] max-w-[1440px] flex-col justify-center',
+      default: 'container',
       sidebar: '',
+      centered: 'container flex h-dvh max-w-2xl flex-col justify-center',
+      markdown: 'container max-w-3xl py-8 md:py-10',
     },
   },
   defaultVariants: {
