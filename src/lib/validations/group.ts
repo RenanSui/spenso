dbfe73ee5765d2557eb709ea4452b96979588104
RenanSui <renanddtao@gmail.com>
2024-05-07 09:06:34 -0300

    extend group validation

--- src/lib/validations/group.ts
+++ src/lib/validations/group.ts
@@ -1,7 +1,11 @@
 import { z } from 'zod'
+import * as zfd from 'zod-form-data'
 
 export const createGroupSchema = z.object({
   title: z.string().min(3).max(50),
 })
-
 export type CreateGroupSchema = z.infer<typeof createGroupSchema>
+
+export const updateGroupSchema = zfd.formData({
+  title: zfd.text(z.string().min(3).max(50)),
+})
