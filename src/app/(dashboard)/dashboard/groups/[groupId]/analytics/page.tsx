a2b2cc4b390eb63e515b790a33243cd4c1cce858
RenanSui <renanddtao@gmail.com>
2024-05-07 09:33:00 -0300

    create groupid analytics page

---
+++ src/app/(dashboard)/dashboard/groups/[groupId]/analytics/page.tsx
@@ -0,0 +1,50 @@
+import { getAllTransactionsRates } from '@/actions/server/currency-rates'
+import { getTransactionsById } from '@/actions/server/transactions'
+import { getGroupById } from '@/actions/server/transactions-groups'
+import { TransactionAnalyticsShell } from '@/components/shells/transactions-analytics-shell'
+import { getTransactionsYears, getTransactionsTypes, getTransactionsCategories } from '@/lib/transactions'
+import { notFound } from 'next/navigation'
+
+type GroupPageProps = {
+  params: {
+    groupId: string
+  }
+}
+
+async function getGroupFromParmas(params: GroupPageProps) {
+  const groupId = params.params.groupId
+
+  const group = await getGroupById(groupId)
+
+  if (!group) return null
+
+  return group
+}
+
+export default async function Page(params: GroupPageProps) {
+  const group = await getGroupFromParmas(params)
+  if (!group) return null
+
+  const transactions = await getTransactionsById(group.id)
+  if (!transactions) notFound()
+
+  const years = getTransactionsYears(transactions)
+  const types = getTransactionsTypes(transactions)
+  const categories = getTransactionsCategories(transactions)
+  const allRates = (await getAllTransactionsRates(transactions)) ?? []
+
+  return (
+    <div className="space-y-6">
+      <div className="flex flex-col gap-4 xs:flex-row xs:items-center xs:justify-between">
+        <h2 className="text-2xl font-bold tracking-tight">{group.title}</h2>
+      </div>
+      <TransactionAnalyticsShell
+        transactions={transactions ?? []}
+        categories={categories}
+        types={types}
+        years={years}
+        allRates={allRates}
+      />
+    </div>
+  )
+}
