83a85b1764239ed3d3d278280e7ea9c8fbd19da5
RenanSui <renanddtao@gmail.com>
2024-05-07 09:33:14 -0300

    create groupid transactions page

---
+++ src/app/(dashboard)/dashboard/groups/[groupId]/transactions/page.tsx
@@ -0,0 +1,40 @@
+import { getAllTransactionsRates } from '@/actions/server/currency-rates'
+import { getTransactionsById } from '@/actions/server/transactions'
+import { getGroupById } from '@/actions/server/transactions-groups'
+import { TransactionsTableShell } from '@/components/shells/transactions-table-shell'
+import { notFound } from 'next/navigation'
+
+type GroupPageProps = {
+  params: {
+    groupId: string
+  }
+}
+
+async function getGroupFromParmas(params: GroupPageProps) {
+  const groupId = params.params.groupId
+
+  const group = await getGroupById(groupId)
+
+  if (!group) return null
+
+  return group
+}
+
+export default async function Page(params: GroupPageProps) {
+  const group = await getGroupFromParmas(params)
+  if (!group) notFound()
+
+  const transactions = await getTransactionsById(group.id)
+  if (!transactions) notFound()
+
+  const allRates = (await getAllTransactionsRates(transactions)) ?? []
+
+  return (
+    <div className="space-y-6">
+      <div className="flex flex-col gap-4 xs:flex-row xs:items-center xs:justify-between">
+        <h2 className="text-2xl font-bold tracking-tight">{group.title}</h2>
+      </div>
+      <TransactionsTableShell groupId={group.id} data={transactions} rates={allRates} />
+    </div>
+  )
+}
