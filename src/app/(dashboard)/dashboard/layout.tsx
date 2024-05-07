0b991fef6ff3ca7d7cf2187b309b218d703430f4
RenanSui <renanddtao@gmail.com>
2024-05-06 23:00:58 -0300

    modify dashboard layout

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,4 +1,5 @@
-import { getTransactionsGroup } from '@/actions/server/transactions-groups'
+import { getGroups } from '@/actions/server/transactions-groups'
+import { CurrencyProvider } from '@/components/providers/currency-provider'
 import { getUser } from '@/lib/auth'
 import { redirect } from 'next/navigation'
 import { DashboardHeader } from './_components/dashboard-header'
@@ -11,25 +12,27 @@ export default async function DashboardLayout({ children }: { children: React.Re
   const user = await getUser()
   if (!user) redirect('/signin')
 
-  const groupsPromise = getTransactionsGroup()
+  const groupsPromise = getGroups()
 
   return (
-    <SidebarProvider>
-      <div className="grid min-h-screen w-full lg:grid-cols-[17.5rem_1fr]">
-        <DashboardSidebar className="top-0 z-30 hidden flex-col gap-4 border-r border-border/60 lg:sticky lg:block">
-          <GroupSwitcher userId={user.id} groupsPromise={groupsPromise} />
-        </DashboardSidebar>
-        <div className="flex flex-col">
-          <DashboardHeader user={user}>
-            <DashboardSidebarSheet>
-              <DashboardSidebar>
-                <GroupSwitcher userId={user.id} groupsPromise={groupsPromise} />
-              </DashboardSidebar>
-            </DashboardSidebarSheet>
-          </DashboardHeader>
-          <main className="flex-1 overflow-hidden px-6">{children}</main>
+    <CurrencyProvider>
+      <SidebarProvider>
+        <div className="grid min-h-screen w-full lg:grid-cols-[17.5rem_1fr]">
+          <DashboardSidebar className="top-0 z-30 hidden flex-col gap-4 border-r border-border/60 lg:sticky lg:block">
+            <GroupSwitcher userId={user.id} groupsPromise={groupsPromise} />
+          </DashboardSidebar>
+          <div className="flex flex-col">
+            <DashboardHeader user={user}>
+              <DashboardSidebarSheet>
+                <DashboardSidebar>
+                  <GroupSwitcher userId={user.id} groupsPromise={groupsPromise} />
+                </DashboardSidebar>
+              </DashboardSidebarSheet>
+            </DashboardHeader>
+            <main className="flex-1 overflow-hidden px-6">{children}</main>
+          </div>
         </div>
-      </div>
-    </SidebarProvider>
+      </SidebarProvider>
+    </CurrencyProvider>
   )
 }
