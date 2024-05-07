06f7e5877b472841da4b14d9abe1aebe4d697c44
RenanSui <renanddtao@gmail.com>
2024-05-07 09:48:48 -0300

    update site header

--- src/components/layouts/site-header.tsx
+++ src/components/layouts/site-header.tsx
@@ -1,7 +1,4 @@
-import { getGroups } from '@/actions/server/transactions-groups'
-import { dashboardConfig } from '@/config/dashboard'
 import { siteConfig } from '@/config/site'
-import { addGroupsToNavbarNav } from '@/lib/utils-config'
 import { User } from '@/types'
 import GroupsCombobox from '../groups-combobox'
 import { AuthDropdown } from './auth-dropdown'
@@ -9,17 +6,11 @@ import { MainNav } from './main-nav'
 import { MobileNav } from './mobile-nav'
 
 export const SiteHeader = async ({ user }: { user: User | null }) => {
-  const transactionsGroups = await getGroups()
-
-  const SidebarNav = transactionsGroups
-    ? await addGroupsToNavbarNav(dashboardConfig.SidebarNav, transactionsGroups)
-    : dashboardConfig.SidebarNav
-
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-white dark:border-neutral-900 dark:bg-neutral-950">
       <div className="container flex h-16 items-center">
         <MainNav items={siteConfig.mainNav} />
-        <MobileNav sidebarNavItems={SidebarNav} />
+        <MobileNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-2">
             <GroupsCombobox />
