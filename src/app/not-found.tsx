9d44e075b74c28b6d6a4babef74164e80ac9409c
RenanSui <renanddtao@gmail.com>
2024-05-07 13:42:14 -0300

    modify app not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,12 @@
 import { ErrorCard } from '@/components/error-card'
 import { Shell } from '@/components/shells/shell'
 
-export default function ProductNotFound() {
+export default function PageNotFound() {
   return (
     <Shell variant="centered" className="max-w-md">
       <ErrorCard
-        title="Product not found"
-        description="The product may have expired or you may have already updated your product"
+        title="Page not found"
+        description="The page you are looking for does not exist"
         retryLink="/"
         retryLinkText="Go to Home"
       />
