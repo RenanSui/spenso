d2c353c3295c58165431c6ca4b2f0b00f7fda426
RenanSui <renanddtao@gmail.com>
2024-05-08 11:43:45 -0300

    create lobby data table components

---
+++ src/app/(lobby)/_components/lobby-data-table.tsx
@@ -0,0 +1,118 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+import { DataTableFilterableColumn, DataTableSearchableColumn } from '@/types'
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from '@tanstack/react-table'
+import React from 'react'
+import { LobbyDataTablePagination } from './lobby-data-table-pagination'
+import { LobbyDataTableToolbar } from './lobby-data-table-toolbar'
+
+interface LobbyDataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[]
+  data: TData[]
+  filterableColumns?: DataTableFilterableColumn<TData>[]
+  searchableColumns?: DataTableSearchableColumn<TData>[]
+  groupId?: string
+}
+
+export function LobbyDataTable<TData, TValue>({
+  columns,
+  data,
+  filterableColumns,
+  searchableColumns,
+  groupId,
+}: LobbyDataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = React.useState<SortingState>([])
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([])
+  const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({})
+  const [rowSelection, setRowSelection] = React.useState({})
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    enableRowSelection: true,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  })
+
+  return (
+    <div className="w-full space-y-3 overflow-auto">
+      <LobbyDataTableToolbar
+        groupId={groupId}
+        table={table}
+        AddNewItem={() => (
+          <Button size="sm" className="px-6">
+            New
+          </Button>
+        )}
+        filterableColumns={filterableColumns}
+        searchableColumns={searchableColumns}
+      />
+      <div className="rounded-md border dark:border-neutral-900">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                    </TableHead>
+                  )
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow className="h-16" key={row.id} data-state={row.getIsSelected() && 'selected'}>
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id} className="text-xs">
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className="h-24 text-center">
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <LobbyDataTablePagination
+        table={table}
+        rows={table.getSelectedRowModel().flatRows}
+        resetRows={table.resetRowSelection}
+      />
+    </div>
+  )
+}
