58e402a8a7e5547b1b0cf49a06000f8a6ecd1c11
RenanSui <renanddtao@gmail.com>
2024-05-09 10:04:58 -0300

    remove comments

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -27,27 +27,8 @@ export default async function Page() {
               </span>
             </div>
           </div>
-          {/* <SignInForm /> */}
         </CardContent>
-        <CardFooter className="flex flex-wrap items-center justify-between gap-2">
-          {/* <div className="text-sm text-neutral-950 dark:text-neutral-400">
-            <span className="mr-1 hidden sm:inline-block">Don&apos;t have an account?</span>
-            <Link
-              aria-label="Sign up"
-              href="/signup"
-              className="text-primary underline-offset-4 transition-colors hover:underline"
-            >
-              Sign up
-            </Link>
-          </div>
-          <Link
-            aria-label="Reset password"
-            href="/signin/reset-password"
-            className="text-primary text-sm underline-offset-4 transition-colors hover:underline"
-          >
-            Reset password
-          </Link> */}
-        </CardFooter>
+        <CardFooter className="flex flex-wrap items-center justify-between gap-2"></CardFooter>
       </Card>
     </Shell>
   )
