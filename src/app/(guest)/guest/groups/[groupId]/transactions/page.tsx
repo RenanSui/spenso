9bc528f999c64231aabaf80c9d0367ed8d1f1441
RenanSui <renanddtao@gmail.com>
2024-05-10 16:06:01 -0300

    add guest groupid transactions page

---
+++ src/app/(guest)/guest/groups/[groupId]/transactions/page.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { notFound } from 'next/navigation'
+import * as React from 'react'
+import { TransactionsContext } from '../../../_components/guest-provider'
+import { GuestTransactionsTableShell } from '../../../_components/guest-transactions-table-shell'
+
+type GroupPageProps = {
+  params: {
+    groupId: string
+  }
+}
+
+export default function Page(params: GroupPageProps) {
+  const groupId = params.params.groupId
+  const guest = React.useContext(TransactionsContext)
+
+  const group = guest.groups.find((group) => group.id === groupId)
+  if (!group) notFound()
+
+  const transactions = guest.transactions.filter((transaction) => transaction.group_id === groupId)
+  const rates = guest.rates
+
+  return (
+    <div className="space-y-6">
+      <div className="flex flex-col gap-4 xs:flex-row xs:items-center xs:justify-between">
+        <h2 className="text-2xl font-bold tracking-tight">{group?.title}</h2>
+      </div>
+
+      <GuestTransactionsTableShell groupId={groupId} data={transactions} rates={rates} />
+    </div>
+  )
+}
