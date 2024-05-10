9b4a8dd775afd3bbf3ee1a94d63658f234ccfa3c
RenanSui <renanddtao@gmail.com>
2024-05-10 01:52:18 -0300

    add userPromise to lobby component

--- src/app/(lobby)/page.tsx
+++ src/app/(lobby)/page.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react'
 
 import { getRate } from '@/actions/server/currency-rates'
+import { getUser } from '@/lib/auth'
 import { mockCategories, mockGroups, mockTransactions } from '@/lib/mocks'
 import { Lobby } from './_components/lobby'
 import { LobbySkeleton } from './_components/lobby-skeleton'
 
 export default async function Page() {
+  const userPromise = getUser()
   const groupsPromise = mockGroups
   const transactionsPromise = mockTransactions
   const categoriesPromise = mockCategories
@@ -14,6 +16,7 @@ export default async function Page() {
   return (
     <React.Suspense fallback={<LobbySkeleton />}>
       <Lobby
+        userPromise={userPromise}
         groupsPromise={groupsPromise}
         transactionsPromise={transactionsPromise}
         categoriesPromise={categoriesPromise}
