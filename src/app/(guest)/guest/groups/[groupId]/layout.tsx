1b9d70edd482aed769fd3d0115ee273c52e3086c
RenanSui <renanddtao@gmail.com>
2024-05-10 16:05:43 -0300

    add guest groups groupid layout

---
+++ src/app/(guest)/guest/groups/[groupId]/layout.tsx
@@ -0,0 +1,24 @@
+import { GuestGroupTabs } from '@/app/(guest)/guest/_components/guest-group-tabs'
+import { PageHeader, PageHeaderDescription, PageHeaderHeading } from '@/components/page-header'
+import { Shell } from '@/components/shells/shell'
+
+interface GroupLayoutProps extends React.PropsWithChildren {
+  params: {
+    groupId: string
+  }
+}
+
+export default async function GroupLayout({ children, params }: GroupLayoutProps) {
+  const groupId = params.groupId
+
+  return (
+    <Shell variant="sidebar" className="gap-4">
+      <PageHeader>
+        <PageHeaderHeading size="sm">Dashboard</PageHeaderHeading>
+        <PageHeaderDescription size="sm">Manage your group</PageHeaderDescription>
+      </PageHeader>
+      <GuestGroupTabs groupId={groupId} />
+      <div className="overflow-hidden">{children}</div>
+    </Shell>
+  )
+}
