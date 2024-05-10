7e56b4cb6162ead19ef8056efe2b300e126174be
RenanSui <renanddtao@gmail.com>
2024-05-10 15:26:43 -0300

    add limit 100 to increase the products size of use product

--- src/hooks/use-products.tsx
+++ src/hooks/use-products.tsx
@@ -6,7 +6,7 @@ export const useProducts = () => {
   return useQuery({
     queryKey: ['dummy-products'],
     queryFn: async () => {
-      const apiURL = 'https://dummyjson.com/products'
+      const apiURL = 'https://dummyjson.com/products?limit=100'
       const res = await fetch(apiURL, { next: { revalidate: false } })
       const data = (await res.json()) as { products: [{ title: string }] }
       const products = data.products.map((item) => item.title).sort()
