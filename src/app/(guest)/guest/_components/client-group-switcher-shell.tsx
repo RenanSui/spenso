2a00e6f626e497a41f5f2e95765c385620fa5c40
RenanSui <renanddtao@gmail.com>
2024-05-15 10:25:18 -0300

    create client group swticher shell

---
+++ src/app/(guest)/guest/_components/client-group-switcher-shell.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { getGroups } from '@/actions/server/transactions-groups'
+import { GroupSwitcher } from '@/app/(dashboard)/dashboard/_components/group-switcher'
+import * as React from 'react'
+import { TransactionsContext } from './guest-provider'
+
+export function ClientGroupSwitcherShell({ userId }: { userId: string }) {
+  const [groupsPromise, setGroupsPromise] = React.useState<ReturnType<typeof getGroups> | null>(null)
+  const { groups } = React.useContext(TransactionsContext)
+
+  React.useEffect(() => {
+    function initLoad() {
+      const groupsPromise = Promise.resolve(groups)
+      setGroupsPromise(groupsPromise)
+    }
+    initLoad()
+  }, [groups])
+
+  return groupsPromise ? <GroupSwitcher route="guest" userId={userId} groupsPromise={groupsPromise} /> : null
+}
