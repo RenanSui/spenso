11e41fe60fd75ab754385c05c75ae3ec5e160bf8
RenanSui <renanddtao@gmail.com>
2024-05-15 09:38:04 -0300

    modify dashboard sidebar props

--- src/app/(dashboard)/dashboard/_components/dashboard-sidebar.tsx
+++ src/app/(dashboard)/dashboard/_components/dashboard-sidebar.tsx
@@ -1,15 +1,17 @@
 import { SidebarNav } from '@/components/layouts/sidebar-nav'
 import { ScrollArea } from '@/components/ui/scroll-area'
-import { dashboardConfig } from '@/config/dashboard'
 import { siteConfig } from '@/config/site'
 import { cn } from '@/lib/utils'
+import { NavItemWithChildren } from '@/types'
 import Link from 'next/link'
+import React from 'react'
 
 interface DashboardSidebarProps extends React.HTMLAttributes<HTMLElement> {
   children: React.ReactNode
+  sidebarItems: NavItemWithChildren[]
 }
 
-export function DashboardSidebar({ children, className, ...props }: DashboardSidebarProps) {
+export function DashboardSidebar({ children, className, sidebarItems, ...props }: DashboardSidebarProps) {
   return (
     <aside className={cn('h-screen w-full', className)} {...props}>
       <div className="hidden h-[3.55rem] items-center border-b border-border/60 px-6 lg:flex">
@@ -17,13 +19,12 @@ export function DashboardSidebar({ children, className, ...props }: DashboardSid
           href="/"
           className="flex w-fit items-center font-heading tracking-wider text-foreground/90 transition-colors hover:text-foreground"
         >
-          {/* <Icons.logo className="mb-1 mr-2 size-6" aria-hidden="true" /> */}
           {siteConfig.name}
         </Link>
       </div>
       <div className="flex flex-col gap-2.5 px-4 pt-2 lg:px-6 lg:pt-4">{children}</div>
       <ScrollArea className="h-[calc(100vh-8rem)] px-3 py-2.5 lg:px-5">
-        <SidebarNav items={dashboardConfig.SidebarNav} className="p-1 pt-4" />
+        <SidebarNav items={sidebarItems} className="p-1 pt-4" />
       </ScrollArea>
     </aside>
   )
