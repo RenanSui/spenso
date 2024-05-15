f31a758f75cfec46b512561588174916dcfac291
RenanSui <renanddtao@gmail.com>
2024-05-15 06:36:44 -0300

    modify theme toggle

--- src/components/layouts/theme-toggle.tsx
+++ src/components/layouts/theme-toggle.tsx
@@ -1,11 +1,12 @@
 'use client'
+
 import { Button } from '@/components/ui/button'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
 import { LaptopIcon, MoonIcon, SunIcon } from '@radix-ui/react-icons'
 import { useTheme } from 'next-themes'
 import { useEffect } from 'react'
 
-export const ThemeToggle = () => {
+export const ThemeToggle = ({ labelled }: { labelled?: boolean }) => {
   const { setTheme, theme } = useTheme()
 
   useEffect(() => {
@@ -15,9 +16,19 @@ export const ThemeToggle = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon" aria-label="theme-toggler">
-          <SunIcon className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Button variant="ghost" size={labelled ? 'default' : 'icon'} aria-label="theme-toggler">
+          {labelled ? (
+            <span className="item-center flex gap-2 capitalize">
+              <SunIcon className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <MoonIcon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              {theme}
+            </span>
+          ) : (
+            <>
+              <SunIcon className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <MoonIcon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            </>
+          )}
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
