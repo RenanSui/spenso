8bb3c7599fa1d9c1e378b095e7b43742e87ab951
RenanSui <renanddtao@gmail.com>
2024-05-15 10:25:44 -0300

    update guest layout

--- src/app/(guest)/guest/layout.tsx
+++ src/app/(guest)/guest/layout.tsx
@@ -1,4 +1,5 @@
 import { DashboardSidebar } from '@/app/(dashboard)/dashboard/_components/dashboard-sidebar'
+import { DashboardSidebarSheet } from '@/app/(dashboard)/dashboard/_components/dashboard-sidebar-sheet'
 import { PageHeaderDescription } from '@/components/page-header'
 import { CurrencyProvider } from '@/components/providers/currency-provider'
 import { guestDashboardConfig } from '@/config/guest'
@@ -6,11 +7,10 @@ import { getUser } from '@/lib/auth'
 import { mockUser } from '@/lib/mocks'
 import Link from 'next/link'
 import { notFound, redirect } from 'next/navigation'
-import { GuestDashboardHeader } from './_components/guest-dashboard-header'
-import { GuestDashboardSidebarSheet } from './_components/guest-dashboard-sidebar-sheet'
-import { GuestGroupSwitcher } from './_components/guest-group-switcher'
+import { ClientGroupSwitcherShell } from './_components/client-group-switcher-shell'
 import { GuestProvider } from './_components/guest-provider'
 import { SidebarProvider } from './_components/guest-sidebar-provider'
+import { DashboardHeader } from '@/app/(dashboard)/dashboard/_components/dashboard-header'
 
 export default async function GuestLayout({ children }: { children: React.ReactNode }) {
   const authUser = await getUser()
@@ -28,16 +28,16 @@ export default async function GuestLayout({ children }: { children: React.ReactN
               className="top-0 z-30 hidden flex-col gap-4 border-r border-border/60 lg:sticky lg:block"
               sidebarItems={guestDashboardConfig.SidebarNav}
             >
-              <GuestGroupSwitcher userId={user.id} />
+              <ClientGroupSwitcherShell userId={user.id} />
             </DashboardSidebar>
             <div className="flex flex-col">
-              <GuestDashboardHeader user={null}>
-                <GuestDashboardSidebarSheet>
+              <DashboardHeader user={null}>
+                <DashboardSidebarSheet>
                   <DashboardSidebar sidebarItems={guestDashboardConfig.SidebarNav}>
-                    <GuestGroupSwitcher userId={user.id} />
+                    <ClientGroupSwitcherShell userId={user.id} />
                   </DashboardSidebar>
-                </GuestDashboardSidebarSheet>
-              </GuestDashboardHeader>
+                </DashboardSidebarSheet>
+              </DashboardHeader>
               <main className="flex-1 overflow-hidden px-6">
                 {children}
                 <PageHeaderDescription size="sm">All your data will be deleted upon close.</PageHeaderDescription>
@@ -54,4 +54,40 @@ export default async function GuestLayout({ children }: { children: React.ReactN
       </CurrencyProvider>
     </GuestProvider>
   )
+  // return (
+  //   <GuestProvider>
+  //     <CurrencyProvider>
+  //       <SidebarProvider>
+  //         <div className="grid min-h-screen w-full lg:grid-cols-[17.5rem_1fr]">
+  //           <DashboardSidebar
+  //             className="top-0 z-30 hidden flex-col gap-4 border-r border-border/60 lg:sticky lg:block"
+  //             sidebarItems={guestDashboardConfig.SidebarNav}
+  //           >
+  //             <GroupSwitcherShell userId={user.id} />
+  //           </DashboardSidebar>
+  //           <div className="flex flex-col">
+  //             <GuestDashboardHeader user={null}>
+  //               <GuestDashboardSidebarSheet>
+  //                 <DashboardSidebar sidebarItems={guestDashboardConfig.SidebarNav}>
+  //                   <div></div>
+  //                   {/* <GroupSwitcherShell userId={user.id} /> */}
+  //                 </DashboardSidebar>
+  //               </GuestDashboardSidebarSheet>
+  //             </GuestDashboardHeader>
+  //             <main className="flex-1 overflow-hidden px-6">
+  //               {children}
+  //               <PageHeaderDescription size="sm">All your data will be deleted upon close.</PageHeaderDescription>
+  //               <PageHeaderDescription size="sm">
+  //                 <Link href="/signin" className="underline transition-all hover:text-foreground">
+  //                   Sign In
+  //                 </Link>{' '}
+  //                 to sync your data.
+  //               </PageHeaderDescription>
+  //             </main>
+  //           </div>
+  //         </div>
+  //       </SidebarProvider>
+  //     </CurrencyProvider>
+  //   </GuestProvider>
+  // )
 }
