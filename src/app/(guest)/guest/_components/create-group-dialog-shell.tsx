4820adbd9eb6626decc516efe2a153025960699b
RenanSui <renanddtao@gmail.com>
2024-05-15 11:32:11 -0300

    add create group dialog shell

---
+++ src/app/(guest)/guest/_components/create-group-dialog-shell.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { CreateGroupDialog } from '@/app/(dashboard)/dashboard/_components/create-group-dialog'
+import * as React from 'react'
+import { TransactionsContext } from './guest-provider'
+
+type CreateGroupDialogShellProps = {
+  userId: string | null | undefined
+}
+
+export function CreateGroupDialogShell({ userId }: CreateGroupDialogShellProps) {
+  const guest = React.useContext(TransactionsContext)
+
+  return <CreateGroupDialog userId={userId} route="guest" createGroup={guest.createGroup} />
+}
