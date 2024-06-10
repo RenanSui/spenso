3a8e05bf2b90f86f4f86f5062b4c99a92a1471f0
RenanSui <renanddtao@gmail.com>
2024-06-10 07:15:54 -0300

    extends icons -> logo icon

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -111,4 +111,22 @@ export const Icons = {
       <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
     </svg>
   ),
+  logo: (props: IconProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      {...props}
+    >
+      <path d="M6 3h12l4 6-10 13L2 9Z" />
+      <path d="M11 3 8 9l4 13 4-13-3-6" />
+      <path d="M2 9h20" />
+    </svg>
+  ),
 }
