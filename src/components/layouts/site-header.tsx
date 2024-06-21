22d1364a6539dec530fe51af34d91c75819c7126
RenanSui <renanddtao@gmail.com>
2024-06-21 20:49:45 -0300

    add transparency to site header

--- src/components/layouts/site-header.tsx
+++ src/components/layouts/site-header.tsx
@@ -7,7 +7,7 @@ import { MobileNav } from './mobile-nav'
 
 export const SiteHeader = async ({ user }: { user: User | null }) => {
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background">
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center">
         <MainNav items={siteConfig.mainNav} />
         <MobileNav items={siteConfig.mainNav} />
