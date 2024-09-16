d14b9d1b3ae32e993bfc0bc1d641df0dd510d2c7
RenanSui <renanddtao@gmail.com>
2024-09-16 13:59:06 -0300

    fix: module warnings

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-
 /** @type {import('next').NextConfig} */
-const nextConfig = withBundleAnalyzer({
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -13,6 +8,6 @@ const nextConfig = withBundleAnalyzer({
       },
     ],
   },
-})
+}
 
-module.exports = nextConfig
+export default nextConfig
--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,6 @@
-module.exports = {
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+const config = {
   plugins: ['prettier-plugin-tailwindcss'],
 }
+
+export default config
