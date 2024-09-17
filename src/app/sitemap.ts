bad52e473406cd428ab132414f537cd05a1df083
RenanSui <renanddtao@gmail.com>
2024-09-17 17:50:17 -0300

    fix: linting

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,18 +1,17 @@
-import { type MetadataRoute } from "next"
+import { type MetadataRoute } from 'next'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-
   const routes = [
-    "",
-    "/dashboard/all-groups",
-    "/dashboard/groups/",
-    "/dashboard/settings/",
-    "/guest/all-groups",
-    "/guest/groups/",
-    "/guest/settings/"
+    '',
+    '/dashboard/all-groups',
+    '/dashboard/groups/',
+    '/dashboard/settings/',
+    '/guest/all-groups',
+    '/guest/groups/',
+    '/guest/settings/',
   ].map((route) => ({
     url: `http://localhost:3000${route}`,
-    lastModified: new Date().toISOString()
+    lastModified: new Date().toISOString(),
   }))
 
   return [...routes]
